4d106d251b9a11bc91b75f9a3205a51b539acd6c
JessaTeal <jessateal@gmail.com>
2022-07-13 15:12:49 -0500

    Create Pokemon List

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
-//create and alert message
-alert('Hello World');
-
-//added favorite food
-let favoriteFood = 'chicken';
-document.write(favoriteFood);
+let pokemonList = [
+  {name: 'Dewgong', height: 1.7, type:['ice','water']},
+  {name: 'Hoppip', height: .4, type:['grass','flying']},
+  {name: 'Gardevoir', height: 1.6, type:['psychic','fairy']},
+];
