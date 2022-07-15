79ceafab3a71bdcfe88a73d3b972d684025fd024
JessaTeal <jessateal@gmail.com>
2022-07-14 23:07:49 -0500

    Created loop for names and height

--- js/script.js
+++ js/script.js
@@ -3,3 +3,7 @@ let pokemonList = [
   {name: 'Hoppip', height: .4, type:['grass','flying']},
   {name: 'Gardevoir', height: 1.6, type:['psychic','fairy']},
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+    document.write(pokemonList[i].name + " is " + pokemonList[i].height + " meters tall! " + "<br>" + "<br>")
+}
