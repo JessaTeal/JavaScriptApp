ecd9cecfe2a6ebc562ede5577f909449b8e0b33f
JessaTeal <jessateal@gmail.com>
2022-07-21 19:51:43 -0500

    task 1.5

--- js/script.js
+++ js/script.js
@@ -1,15 +1,37 @@
 let pokemonList = [
-  {name: 'Dewgong', height: 1.7, type:['ice','water']},
-  {name: 'Hoppip', height: .4, type:['grass','flying']},
-  {name: 'Gardevoir', height: 1.6, type:['psychic','fairy']},
+  {
+      name: 'Dewgong',
+      height: 1.7,
+      type: ['ice','water']
+  },
+  {
+      name: 'Hoppip',
+      height: .4,
+      type: ['grass','flying']
+  },
+  {
+      name: 'Gardevoir',
+      height: 1.6,
+      type: ['psychic','fairy']
+  }
 ];
 
-for (let i = 0; i < pokemonList.length; i++) {
-  if (pokemonList[i].height > 1.5){
-    document.write(pokemonList[i].name + " is " + pokemonList[i].height + " meters tall! " + " That's so tall!" + "<br>" + "<br>")
-  }else if (pokemonList[i].height < .5){
-    document.write(pokemonList[i].name + " is " + pokemonList[i].height + " meters tall! " + " It's so tiny!"+ "<br>" + "<br>")
-  }else {
-    document.write(pokemonList[i].name + " is " + pokemonList[i].height + " meters tall! " + "<br>" + "<br>")
-  }
-}
+let pokemonRepository = (function () {
+
+  let pokemonList = []
+
+
+  return {
+    add: function(pokemon) {
+      pokemonList.push(pokemon);
+    },
+    getAll: function() {
+      return pokemonList;
+    }
+  };
+
+})();
+
+document.write(pokemonRepository.getAll());
+pokemonRepository.add({ name: 'Pikachu', height: 1.7, type:['psychic', 'grass'] });
+document.write(pokemonRepository.getAll());
