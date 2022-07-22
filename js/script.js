d7f71e92687a17b22ba50e8acf456397cc6f2bd7
JessaTeal <jessateal@gmail.com>
2022-07-21 19:52:54 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,24 +1,22 @@
-let pokemonList = [
-  {
-      name: 'Dewgong',
-      height: 1.7,
-      type: ['ice','water']
-  },
-  {
-      name: 'Hoppip',
-      height: .4,
-      type: ['grass','flying']
-  },
-  {
-      name: 'Gardevoir',
-      height: 1.6,
-      type: ['psychic','fairy']
-  }
-];
-
 let pokemonRepository = (function () {
 
-  let pokemonList = []
+  let pokemonList = [
+    {
+        name: 'Dewgong',
+        height: 1.7,
+        type: ['ice','water']
+    },
+    {
+        name: 'Hoppip',
+        height: .4,
+        type: ['grass','flying']
+    },
+    {
+        name: 'Gardevoir',
+        height: 1.6,
+        type: ['psychic','fairy']
+    }
+  ];
 
 
   return {
