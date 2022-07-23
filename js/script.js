225db8a90dde1fd3a49783f9907385707831c667
JessaTeal <jessateal@gmail.com>
2022-07-22 23:24:16 -0500

    update for 1.5

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 
-let pokemonList = [
-  {name: 'Dewgong', height: 1.7, type:['ice','water']},
-  {name: 'Hoppip', height: .4, type:['grass','flying']},
-  {name: 'Gardevoir', height: 1.6, type:['psychic','fairy']},
-];
+//let pokemonList = [
+  //{name: 'Dewgong', height: 1.7, type:['ice','water']},
+  //{name: 'Hoppip', height: .4, type:['grass','flying']},
+  //{name: 'Gardevoir', height: 1.6, type:['psychic','fairy']},
+//];
 
-for (let i = 0; i < pokemonList.length; i++) {
-  if (pokemonList[i].height > 1.5){
-    document.write(pokemonList[i].name + " is " + pokemonList[i].height + " meters tall! " + " That's so tall!" + "<br>" + "<br>")
-  }else if (pokemonList[i].height < .5){
-    document.write(pokemonList[i].name + " is " + pokemonList[i].height + " meters tall! " + " It's so tiny!"+ "<br>" + "<br>")
-  }else {
-    document.write(pokemonList[i].name + " is " + pokemonList[i].height + " meters tall! " + "<br>" + "<br>")
-  }
-}
+//for (let i = 0; i < pokemonList.length; i++) {
+  //if (pokemonList[i].height > 1.5){
+    //document.write(pokemonList[i].name + " is " + pokemonList[i].height + " meters tall! " + " That's so tall!" + "<br>" + "<br>")
+  //}else if (pokemonList[i].height < .5){
+    //document.write(pokemonList[i].name + " is " + pokemonList[i].height + " meters tall! " + " It's so tiny!"+ "<br>" + "<br>")
+  //}else {
+    //document.write(pokemonList[i].name + " is " + pokemonList[i].height + " meters tall! " + "<br>" + "<br>")
+  //}
+//}
 
 
 let pokemonRepository2 = (function () {
@@ -21,8 +21,9 @@ let pokemonRepository2 = (function () {
   let pokemonList = [
     {name: 'Dewgong', height: 1.7, type:['ice','water']},
     {name: 'Hoppip', height: .4, type:['grass','flying']},
-    {name: 'Gardevoir', height: 1.6, type:['psychic','fairy']},
+    {name: 'Gardevoir', height: 1.6, type:['psychic','fairy']}
   ];
+
 
   return {
     add: function(pokemon) {
@@ -30,11 +31,12 @@ let pokemonRepository2 = (function () {
     },
     getAll: function() {
       return pokemonList;
-    }
+    },
   };
 
 })();
 
-document.write(pokemonRepository2.getAll());
 pokemonRepository2.add({ name: 'Pikachu', height: 2.7, type: ['grass','fairy']});
-document.write(pokemonRepository2.getAll());
+pokemonRepository2.getAll().forEach(function(pokemonList) {
+  document.write('<p>' + pokemonList.name + ' is ' + pokemonList.height + ' meters tall and is ' + pokemonList.type + ' types! </p>');
+});
